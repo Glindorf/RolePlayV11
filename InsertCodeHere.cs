214d3a477a7844b55ea4c893dae3e146fe14d259
Madeleine Glindorf <mglindorf@gmail.com>
2020-03-30 13:27:43 +0200

    Initial commit

---
+++ InsertCodeHere.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace RolePlayV11
+{
+    public class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+
+            Warrior warriorA = new Warrior("Ragnar", 50);
+            Warrior warriorB = new Warrior("Lagertha", 50);
+
+            System.Console.WriteLine($"{warriorA.Name}'s points: {warriorA.HitPoints}");
+            System.Console.WriteLine($"Is {warriorA.Name} dead? {warriorA.Dead}");
+            warriorA.DealDamage();
+            System.Console.WriteLine($"{warriorA.Name}'s points: {warriorA.HitPoints}");
+            System.Console.WriteLine($"Is {warriorA.Name} dead? {warriorA.Dead}");
+            warriorA.DealDamage();
+            System.Console.WriteLine($"{warriorA.Name}'s points: {warriorA.HitPoints}");
+            System.Console.WriteLine($"Is {warriorA.Name} dead? {warriorA.Dead}");
+            warriorA.DealDamage();
+            System.Console.WriteLine($"{warriorA.Name}'s points: {warriorA.HitPoints}");
+            System.Console.WriteLine($"Is {warriorA.Name} dead? {warriorA.Dead}");
+
+            Console.WriteLine($"Warrior A is called {warriorA.Name} is level {warriorA.Level}");
+            Console.WriteLine($"Warrior B is called {warriorB.Name} is level {warriorB.Level}");
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
---
+++ Warrior.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace RolePlayV11
+{
+    public class Warrior
+    {
+        #region Instance fields
+        private string _name;
+        private int _level;
+        private int _hitPoints;
+        #endregion
+
+        #region Constructor
+        public Warrior(string name, int hitPoints)
+        {
+            _name = name;
+            _level = 1;
+            _hitPoints = hitPoints;
+        }
+        #endregion
+
+        #region Properties
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public int Level
+        {
+            get { return _level; }
+        }
+
+        public int HitPoints
+        {
+            get { return _hitPoints; }
+        }
+
+        public bool Dead
+        {
+            get { return _hitPoints <= 0; }
+        }
+        #endregion
+
+        #region Methods
+        public void LevelUp()
+        {
+            _level = _level + 1;
+        }
+
+        public void ReceiveDamage(int damageMinus)
+        {
+            _hitPoints = _hitPoints - damageMinus;
+        }
+
+        public int DealDamage()
+        {
+            int dmg = new Random().Next(10, 31);
+            _hitPoints = _hitPoints - dmg;
+            return _hitPoints;
+        }
+
+        #endregion
+
+    }
+}
